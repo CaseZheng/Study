8c568ab3741be68d124c950f7a15c1ff445fa681
CaseZheng <764307915@qq.com>
2017-08-27 23:49:30 +0800

    factory

---
+++ DesignPattern/factory.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+//工厂模式
+class Product
+{
+public:
+    virtual ~Product(){};
+protected:
+    Product(){};
+};
+
+class ConcreteProduct:public Product
+{
+public:
+    ConcreteProduct()
+    {
+        cout<<__FUNCTION__<<endl;
+    }
+    ~ConcreteProduct()
+    {
+    };
+};
+
+class Factory
+{
+public:
+    virtual ~Factory(){};
+    virtual void* CreateProduct() = 0;
+protected:
+    Factory(){};
+};
+
+class ConcreteFactory:public Factory
+{
+public:
+    ConcreteFactory()
+    {
+        cout<<__FUNCTION__<<endl;
+    }
+    ~ConcreteFactory()
+    {
+    }
+    Product* CreateProduct()
+    {
+        return new ConcreteProduct();
+    }
+};
+
+int main()
+{
+    Factory *fac = new ConcreteFactory();
+    Product *p = (Product*)fac->CreateProduct();
+    return 0;
+}
