c4a4d7dc0ad6779a653e27f7aa38c12a1dca1bc4
CaseZheng <764307915@qq.com>
2019-02-28 13:37:23 +0800

    并发复习

---
+++ Concurrent/process_study.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+
+#if 1
+int main()
+{
+    cout << "start fork" << " PID:" << getpid() << endl;
+    pid_t iPid = fork();
+    if(iPid < 0)
+    {
+        cout << "fork error" << " PID:" << getpid() << endl;
+    }
+    else if (iPid == 0)
+    {
+        cout << "child process" << " PID:" << getpid() << " PPID:" << getppid() << endl;
+    }
+    else
+    {
+        cout << "parent process" << " PID:" << getpid() << endl;
+    }
+
+    sleep(300);
+
+    return 0;
+}
+#endif
