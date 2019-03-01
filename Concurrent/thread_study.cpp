f11c9ac84a8de1f7e78689b4fa389ae41504f795
CaseZheng <764307915@qq.com>
2019-03-01 15:08:10 +0800

    thread

--- Concurrent/thread_study.cpp
+++ Concurrent/thread_study.cpp
@@ -2,10 +2,43 @@
 #include <string>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/types.h>
+#include <boost/thread/thread.hpp>
+#include <boost/chrono.hpp>
+#include <boost/ref.hpp>
 
 using namespace std;
 
+void Print1(int *pCount)
+{
+    for(;(*pCount)<10;++(*pCount))
+    {
+        cout << "PID:" << getpid() << " TID:" << pthread_self() << "  " << *pCount << endl;
+        boost::this_thread::sleep_for(boost::chrono::seconds(1));
+    }
+}
+
+void Print2(int &count)
+{
+    for(;count<10;++count)
+    {
+        cout << "PID:" << getpid() << " TID:" << pthread_self() << "  " << count << endl;
+        boost::this_thread::sleep_for(boost::chrono::seconds(1));
+    }
+}
+
+int main()
+{
+    int count = 1;
+    boost::thread t1(Print1, &count);
+    boost::thread t2(Print2, boost::ref(count));
+    cout << "t1 id:" << t1.get_id() << endl;
+    cout << "t2 id:" << t2.get_id() << endl;
+    t1.join();
+    t2.join();
+    return 0;
+}
+
+#if 0
 void *Print(void *pArg)
 {
     int *pCount = (int*)pArg;
@@ -39,3 +72,4 @@ int main()
 
     return 0;
 }
+#endif
